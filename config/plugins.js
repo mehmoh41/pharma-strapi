17794a36664376bef2fdc7e99559e96e55f497a8
mehmoh41 <moh.meh41@gmail.com>
2023-07-21 08:30:57 +0500

    new changes added

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,10 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_UFcV8oWuxpkfR5alyQarcXlR7R2Y/dWisEHRUYO",
+      apiToken: "MmH4kY3ZhbAbj8WiNgTQSjZ6",
+      appFilter: "pharma-strapi",
       // teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
       roles: ["strapi-super-admin"],
     },
